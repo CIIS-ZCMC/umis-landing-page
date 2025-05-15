e3a6e4c59fcb5cb82740df073ea864587296feb0
TjayAmit <tristanjayamit0813@gmail.com>
2025-05-15 14:01:53 +0800

    added ERP

--- src/services/services.jsx
+++ src/services/services.jsx
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const BASE_URL = {
   production: "https://umis.zcmc.online/api/",
-  development: "http://192.168.5.1:8100/api/",
+  development: "http://192.168.9.243:8010/api/",
   local: "http://localhost:8000/api/",
 };
 
 const umis = new axios.create({
-  baseURL: BASE_URL.local,
+  baseURL: BASE_URL.development,
   withCredentials: true,
   headers: {
     Accept: "application/json",
