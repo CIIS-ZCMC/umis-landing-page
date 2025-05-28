d4235ac5236f8a2c740d4def7aac7bd6a977b015
TjayAmit <tristanjayamit0813@gmail.com>
2025-05-28 11:39:57 +0800

    update

--- src/components/StartPageBody/StartPageBody.jsx
+++ src/components/StartPageBody/StartPageBody.jsx
@@ -80,7 +80,7 @@ const StartPage = () => {
 </svg>`,
     },
     {
-      title: "Enterprice Resource Planning",
+      title: "Enterprise Resource Planning",
       description: "Oversee your annual plan for next year.",
       link: retrieveAdminAccess("ERP"),
       icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
