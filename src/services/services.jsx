414565fd846713c73aafc350061cc94e414139f9
TjayAmit <tristanjayamit0813@gmail.com>
2026-04-07 11:40:01 +0800

    feat: added staging environment

--- src/services/services.jsx
+++ src/services/services.jsx
@@ -2,12 +2,15 @@ import axios from "axios";
 
 const BASE_URL = {
   production: "https://umis.zcmc.online/api/",
+  staging: "https://staging-umis.zcmc.online/api/",
   development: "http://192.168.36.150:81/api/",
   local: "http://192.168.36.150:81/api/",
 };
 
+export const ENV = 'staging';
+
 const umis = new axios.create({
-  baseURL: BASE_URL.development,
+  baseURL: BASE_URL[ENV],
   withCredentials: true,
   headers: {
     Accept: "application/json",
@@ -15,4 +18,5 @@ const umis = new axios.create({
   },
 });
 
-export { umis };
+export { umis };cls
+
